a990d34686aba5f22bc877183000637ef0231e24
minthangml <thangpi4216@gmail.com>
2020-08-17 12:06:40 +0630

    Second Commit

--- app/src/main/java/com/hamuli/hamulimeeting/activities/SignInActivity.java
+++ app/src/main/java/com/hamuli/hamulimeeting/activities/SignInActivity.java
@@ -1,12 +1,20 @@
 package com.hamuli.hamulimeeting.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.hamuli.hamulimeeting.R;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -16,5 +24,15 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         findViewById(R.id.textSignUp).setOnClickListener(view -> startActivity(new Intent(SignInActivity.this, SignUpActivity.class)));
+
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        HashMap<String, Object> user = new HashMap<>();
+//        user.put("first_name", "John");
+//        user.put("last_name", "Doe");
+//        user.put("email", "john.doe@gmail.com");
+//        database.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(documentReference -> Toast.makeText(SignInActivity.this, "User Added", Toast.LENGTH_SHORT).show())
+//                .addOnFailureListener(e -> Toast.makeText(SignInActivity.this, "Error adding user", Toast.LENGTH_SHORT).show());
     }
 }
